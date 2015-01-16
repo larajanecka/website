4e1e0d223ad2610fe123c8d947f57709ac598720
Lara <numbersandlettersandallthat@gmail.com>
2015-01-16 12:23:28 -0500

    Text added to cube

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -3,6 +3,26 @@
         var camera, scene, renderer, controls, raycaster, mouse;
 
         var links = ['/about', '/resume', '/skills', '/soft', '/experience', '/contact'];
+        var text = [{
+                title: "About",
+                description: "A brief description of me"
+            },{
+                title: "Resume",
+                description: "Download my resume"
+            },{
+                title: "Skills",
+                description: "An description of what I can do"
+            },{
+                title: "Soft Skills",
+                description: "A description of what I can do away from a computer"
+            },{
+                title: "Experience",
+                description: "A description of what I have done"
+            },{
+                title: "Contact",
+                description: "Various methods of contacting me"
+            }
+        ]
 
         init();
         animate();
@@ -31,32 +51,33 @@
             scene.add( light );
 
 
-            var geometry = new THREE.BoxGeometry(200, 200, 200);
             raycaster = new THREE.Raycaster();
             mouse = new THREE.Vector2();
 
-            for (var i = 0, j = 0; i < geometry.faces.length; i += 2, j ++) {
 
-                var hex = Math.random() * 0xffffff;
-                geometry.faces[ i ].color.setHex(hex);
-                geometry.faces[ i + 1 ].color.setHex(hex);
+
+            var materials = [];
+            for ( var i = 0; i < 6; i ++ ) {
+                materials.push( new THREE.MeshBasicMaterial( {map: getTexture(i) } ) );
+            }
+
+
+            var geometry = new THREE.BoxGeometry(200, 200, 200);
+            for (var i = 0, j = 0; i < geometry.faces.length; i += 2, j ++) {;
                 geometry.faces[i].link = links[j];
                 geometry.faces[i + 1].link = links[j];
 
             }
 
-
-
-            var material = new THREE.MeshBasicMaterial( { vertexColors: THREE.FaceColors, overdraw: 0.5 } );
-
-                cube = new THREE.Mesh( geometry, material );
-                scene.add( cube );
+            cube = new THREE.Mesh( geometry, new THREE.MeshFaceMaterial(materials));
+            scene.add( cube );
 
 
             document.addEventListener('dblclick', onDocumentClick)
             window.addEventListener('resize', onWindowResize)
 
         }
+
 
         function animate() {
 
@@ -85,5 +106,31 @@
 
             var intersects = raycaster.intersectObjects([cube]);
             window.location = intersects[0].face.link;
+    }
+
+    function getTexture(index) {
+        var hex = "#" + Math.random().toString(16).slice(2, 8);
+
+        var bitmap = document.createElement('canvas');
+        var g = bitmap.getContext('2d');
+
+        bitmap.width = 200;
+        bitmap.height = 200;
+
+        g.fillStyle = hex;
+        g.fillRect(0,0,200,200);
+
+        g.font = 'Bold 20px Arial';
+        g.fillStyle = 'white';
+        g.textAlign = 'center'
+        g.fillText(text[index].title, 100, 100);
+
+        g.font = 'Bold 10px Arial';
+        g.fillText(text[index].description, 100, 120);
+
+        var texture = new THREE.Texture(bitmap)
+        texture.needsUpdate = true;
+        return texture;
+
     }
 
